863054f10192ec98a6f0f667cd278b53160926c8
Cole Botsford <botsford.4@osu.edu>
2020-02-12 00:02:13 -0500

    init check

---
+++ src/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default fuction Home() {
+    return (
+        <div className="Home">
+         <div className="lander">
+            <hl>Home</hl>
+            <p>Welcome to Fisher Bookstore.</p>
+        </div>
+     </div>
+    );
+}
