35b914847616d842881703b23da762943735215f
colebots <43837965+colebots@users.noreply.github.com>
2020-02-19 18:17:59 -0500

    Project structure (#20)
    * init the project
    
    * init all
    
    * init commit more
    
    * init check
    
    * init commit nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Routes from "./Routes.js";
 
 export default class App extends Component {
-  render()  {
-    return <div>Fisher Bookstore</div>;
+  render() {
+    return (
+      <div ClassName="App container">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>
+            <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav>
+              <NavItem>
+                <Nav.Link href="/authors">Books</Nav.Link>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
