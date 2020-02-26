8b87b4fd860a18ff96073e2c68310c187526a4c1
Cole Botsford <botsford.4@osu.edu>
2020-02-26 17:55:43 -0500

    books-committed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Navbar } from "react-bootstrap";
+import { Nav, NavItem, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default class App extends Component {
--- src/books/Books.js
+++ src/books/Books.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Books.css";
 import { getDefaultNormalizer, render } from "@testing-library/react";
 
-render() {
+render() 
     return (
         <div className="Books">
             <div className="lander">
@@ -24,9 +24,33 @@ constructor(props) {
             },
             {
                 id: 2,
+                title: "Harry Potter and the Deathly Hallows",
+                author: "J.K. Rowling",
+                isbn: "978-0605039070"
+            },
+            {
+                id: 3,
+                title: "Harry Potter and the Sorceror's Stone",
+                author: "J.K. Rowling",
+                isbn: "978-0590353427"
+            },
+            {
+                id: 4,
                 title: "Accelerate",
                 author: "Nicole Forsgren",
                 isbn: "978-1942788331"
+            },
+            {
+                id: 5,
+                title: "The Lord of the Rings",
+                author: "J.R.R. Tolkien",
+                isbn: "978-0544003415"
+            },
+            {
+                id: 6,
+                title: "The Hobbit",
+                author: "J.R.R. Tolkien",
+                isbn: "978-0547928227"
             }
         ]
     };
